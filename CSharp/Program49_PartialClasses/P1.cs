48d91dad20aab708222a07289b5656f86358c041
Summy Singh <summysingh86@gmail.com>
2020-09-16 22:21:38 +0530

    one program added

---
+++ CSharp/Program49_PartialClasses/P1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program49_PartialClasses
+{
+    public partial class Geeks
+    {
+        private string name;
+        private int total_article;
+
+        public Geeks(string a, int t)
+        {
+            this.name = a;
+            this.total_article = t;
+        }
+    }
+}
---
+++ CSharp/Program49_PartialClasses/P2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program49_PartialClasses
+{
+    public partial class Geeks
+    {
+        public void display()
+        {
+            Console.WriteLine("Name : {0}", name);
+            Console.WriteLine("Articles : {0}", total_article);
+        }
+    }
+}
