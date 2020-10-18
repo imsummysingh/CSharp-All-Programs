9c4618b6e027f2c23959925bfbaddbfc9b60ccea
Summy Singh <summysingh86@gmail.com>
2020-10-18 23:59:10 +0530

    program added

---
+++ CSharp/Program75_MethodHiding/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program75_MethodHiding
+{
+    public class My_family
+    {
+        public void member()
+        {
+            Console.WriteLine("Total no. of family member is 3");
+        }
+    }
+    public class My_Member : My_family
+    {
+        //public new void member()
+        //{
+        //    Console.WriteLine("This is family");
+        //}
+        public void member()
+        {
+            //to call the hidden base class method
+            //base.member();
+            Console.WriteLine("This is family");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            My_Member obj = new My_Member();
+            obj.member();
+
+            //type casting to check the hidden methhod from base class
+            //((My_family)obj).member();
+
+            Console.ReadLine();
+        }
+    }
+}
