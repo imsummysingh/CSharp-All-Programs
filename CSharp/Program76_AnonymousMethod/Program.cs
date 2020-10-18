3deda991409b585b052f04b9391d2513a2b0010e
Summy Singh <summysingh86@gmail.com>
2020-10-19 00:16:18 +0530

    delegate

---
+++ CSharp/Program76_AnonymousMethod/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program76_AnonymousMethod
+{
+    class Program
+    {
+        public delegate void petname(string pet);
+        static void Main(string[] args)
+        {
+            petname p = delegate (string mypet)
+            {
+                Console.WriteLine("My Fav Pet is: {0}", mypet);
+            };
+           p("Fish");
+            Console.ReadLine();
+        }
+    }
+}
