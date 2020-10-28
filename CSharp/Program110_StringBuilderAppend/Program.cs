d0babfa3387063a839f1ca037cc29a21601d6a46
Summy Singh <summysingh86@gmail.com>
2020-10-28 21:13:50 +0530

    string builder

---
+++ CSharp/Program110_StringBuilderAppend/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program110_StringBuilderAppend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder s = new StringBuilder("Hello", 20);
+            s.Append("Summy");
+            s.AppendLine("Singh");
+            s.Append("summy singh");
+            Console.WriteLine(s);
+            Console.ReadLine();
+        }
+    }
+}
